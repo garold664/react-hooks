b4e2b990a014045793cbb3e7f952bf36b08a7ad4
garold664 <garold664@gmail.com>
2024-06-06 07:56:57 +0300

    set up second problem that useMemo can fix: referential equality and dependency array

--- src/pages/UseMemoPage.tsx
+++ src/pages/UseMemoPage.tsx
@@ -1,14 +1,30 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 
 export default function UseMemoPage() {
   const [number, setNumber] = useState(0);
   const [dark, setDark] = useState(false);
+  // const [themeChangeCounter, setThemeChangeCounter] = useState(0);
+  const themeChangeCounter = useRef(0);
   //# 1 reason to use useMemo
   // slowFunction(number);
   // if use slowFunction without useMemo the change of theme happens as slow as the change of number.
   // but with useMemo the change of theme happens fast because it shouldn't change the number.
   // use useMemo only if you have really slow calculations that you need to memoize.
   const doubleNumber = useMemo(() => slowFunction(number), [number]);
+
+  //# 2 reason to use useMemo
+  //% referential equality
+  const themeStyles = {
+    backgroundColor: dark ? 'black' : 'white',
+    color: dark ? 'white' : 'black',
+    padding: '2rem',
+  };
+
+  useEffect(() => {
+    // setThemeChangeCounter((prev) => prev + 1); // this would cause infinite loop
+    themeChangeCounter.current += 1;
+    console.log('changing theme');
+  }, [themeStyles]);
 
   return (
     <>
@@ -20,14 +36,20 @@ export default function UseMemoPage() {
         onChange={(e) => setNumber(parseInt(e.target.value))}
       />
       <button onClick={() => setDark((prev) => !prev)}>Toggle Theme</button>
+      <div style={themeStyles}>{doubleNumber}</div>
       <div
         style={{
-          backgroundColor: dark ? 'black' : 'white',
-          color: dark ? 'white' : 'black',
           padding: '2rem',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: '2rem',
         }}
       >
-        {doubleNumber}
+        Theme Change Counter:{' '}
+        <span style={{ fontWeight: 'bold', color: 'red', fontSize: '3rem' }}>
+          {themeChangeCounter.current}
+        </span>
       </div>
     </>
   );
