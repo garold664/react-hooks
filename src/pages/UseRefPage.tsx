0097729cf2bc5bac17e2b637e8cf8888df409a45
garold664 <garold664@gmail.com>
2024-06-24 19:22:25 +0300

    uses useRef to focus the input

--- src/pages/UseRefPage.tsx
+++ src/pages/UseRefPage.tsx
@@ -4,6 +4,7 @@ export default function UseRefPage() {
   const [name, setName] = useState('');
   // const [renderCount, setRenderCount] = useState(0);
   const renderCount = useRef(1);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   React.useEffect(() => {
     // setRenderCount(renderCount + 1); // this causes infinite loop of rerenders
@@ -16,9 +17,11 @@ export default function UseRefPage() {
         type="text"
         value={name}
         onChange={(e) => setName(e.target.value)}
+        ref={inputRef}
       />
       <div>My name is {name}</div>
       <div>I rendered {renderCount.current} times</div>
+      <button onClick={() => inputRef.current?.focus()}>focus</button>
     </>
   );
 }
