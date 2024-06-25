750f5a3bafb9332da37d5740a6cf727ded12ce9e
garold664 <garold664@gmail.com>
2024-06-25 22:26:36 +0300

    adds useRef with a Map saved in

--- src/pages/UseRef2Page/UseRef2Page.tsx
+++ src/pages/UseRef2Page/UseRef2Page.tsx
@@ -1,20 +1,48 @@
 import { useRef, useState } from 'react';
 
 export default function UseRef2Page() {
-  const itemsRef = useRef(null);
+  const itemsRef = useRef<Map<string, HTMLLIElement>>(null);
   const [catList, setCatList] = useState(setupCatList);
+
+  function scrollToCat(cat: string) {
+    const map = getMap();
+    const node = map.get(cat);
+    node?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'center',
+    });
+  }
+
+  function getMap() {
+    if (!itemsRef.current) {
+      // Initialize the Map on first usage.
+      itemsRef.current = new Map();
+    }
+    return itemsRef.current;
+  }
 
   return (
     <>
       <nav>
-        <button>Tom</button>
-        <button>Maru</button>
-        <button>Jellylorum</button>
+        <button onClick={() => scrollToCat(catList[0])}>Tom</button>
+        <button onClick={() => scrollToCat(catList[5])}>Maru</button>
+        <button onClick={() => scrollToCat(catList[9])}>Jellylorum</button>
       </nav>
       <div>
         <ul>
           {catList.map((cat) => (
-            <li key={cat}>
+            <li
+              key={cat}
+              ref={(node) => {
+                const map = getMap();
+                if (node) {
+                  map.set(cat, node);
+                } else {
+                  map.delete(cat);
+                }
+              }}
+            >
               <img src={cat} />
             </li>
           ))}
