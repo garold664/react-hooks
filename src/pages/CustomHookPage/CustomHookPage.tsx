fb285c72f2112475127e027a2c081283142c52a2
garold664 <garold664@gmail.com>
2024-06-27 11:59:39 +0300

    creates simple useUpdate custom hook

--- src/pages/CustomHookPage/CustomHookPage.tsx
+++ src/pages/CustomHookPage/CustomHookPage.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import useLocalStorage from './useLocalStorage';
+import useUpdateLogger from './useUpdateLogger';
 
 export default function CustomHookPage() {
   const [name, setName] = useLocalStorage('name', '');
+  useUpdateLogger(name);
   return (
     <>
       <h1>CustomHookPage</h1>
