ceb24120f47db5887902dfe73d0b5c6bddc483bb
garold664 <garold664@gmail.com>
2024-10-01 16:47:57 +0300

    add useDebugValue hook to useLocalStorage custom hook

--- src/pages/CustomHookPage/useLocalStorage.tsx
+++ src/pages/CustomHookPage/useLocalStorage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useDebugValue, useEffect, useState } from 'react';
 
 function getSavedValue(key: string, initialValue: any) {
   const savedValue = localStorage.getItem(key);
@@ -13,9 +13,17 @@ function getSavedValue(key: string, initialValue: any) {
 export default function useLocalStorage(key: string, initialValue: string) {
   const [value, setValue] = useState(() => getSavedValue(key, initialValue));
 
+  useDebugValue(key);
+  useDebugValue(value, () => slowFunction(value));
+
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
-  }, [value]);
+  }, [value, key]);
 
   return [value, setValue] as const;
+}
+
+function slowFunction(value: any) {
+  for (let i = 0; i < 3 * 10 ** 9; i++) {}
+  return value;
 }
